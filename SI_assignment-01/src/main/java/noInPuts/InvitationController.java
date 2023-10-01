15b1c7e7b782d3f322b549a73251b027d7ad555d
Benjamin S <39574422+pl0xi@users.noreply.github.com>
2023-10-01 22:22:34 +0200

    Get Location by IP

--- SI_assignment-01/src/main/java/noInPuts/InvitationController.java
+++ SI_assignment-01/src/main/java/noInPuts/InvitationController.java
@@ -1,13 +1,67 @@
 package noInPuts;
 
 import jdk.jshell.spi.ExecutionControl;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class InvitationController {
-    public static void getGeoLocationByIP(String ip) throws ExecutionControl.NotImplementedException {
+    public static String getGeoLocationByIP(String ip) throws ExecutionControl.NotImplementedException {
         if(ip.isEmpty()) {
             throw new IllegalArgumentException("Missing argument");
         }
 
+        try {
+            URL url = new URL("http://wsgeoip.lavasoft.com/ipservice.asmx");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "text/xml");
+            connection.setDoOutput(true);
+
+            String xmlPayload = String.format("<?xml version='1.0' encoding='utf-8' ?>" +
+                    "<soap12:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap12='http://www.w3.org/2003/05/soap-envelope'>" +
+                        "<soap12:Body>" +
+                            "<GetIpLocation xmlns='http://lavasoft.com/'>" +
+                                "<sIp>%s</sIp>" +
+                            "</GetIpLocation>" +
+                        "</soap12:Body>" +
+                    "</soap12:Envelope>", ip);
+
+            try (OutputStream os = connection.getOutputStream();
+                 OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8")) {
+                osw.write(xmlPayload);
+                osw.flush();
+            }
+
+            int responseCode = connection.getResponseCode();
+
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+                DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+                Document doc = dBuilder.parse(connection.getInputStream());
+
+                // Extract the IP address from the XML
+                NodeList nodeList = doc.getElementsByTagName("GetIpLocationResult");
+                if (nodeList.getLength() > 0) {
+                    String xmlResult = nodeList.item(0).getTextContent();
+                    Document resultDoc = dBuilder.parse(new java.io.ByteArrayInputStream(xmlResult.getBytes("UTF-8")));
+                    connection.disconnect();
+                    return resultDoc.getElementsByTagName("Country").item(0).getTextContent();
+                }
+            }
+
+            connection.disconnect();
+            throw new IllegalArgumentException("Invalid IP address");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         throw new ExecutionControl.NotImplementedException("Not implemented");
     }
 
--- SI_assignment-01/src/test/java/InvitationControllerTest.java
+++ SI_assignment-01/src/test/java/InvitationControllerTest.java
@@ -1,2 +1,21 @@
+import jdk.jshell.spi.ExecutionControl;
+import noInPuts.InvitationController;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class InvitationControllerTest {
+
+    @Test
+    public void testGetGeoLocationByIP() throws ExecutionControl.NotImplementedException {
+        InvitationController invitationController = new InvitationController();
+        String result = invitationController.getGeoLocationByIP("195.47.247.8");
+        assertEquals("DK", result);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetGeoLocationByIPWithEmptyString() throws ExecutionControl.NotImplementedException {
+        InvitationController invitationController = new InvitationController();
+        invitationController.getGeoLocationByIP("");
+    }
 }
